36626152cfa96ab8f0f2a3c443416dfddb56cdad
Dev122-coder <pramanikd45@gmail.com>
2026-04-18 17:28:39 +0530

    done

---
+++ index.js
@@ -0,0 +1,45 @@
+var p = document.querySelector(".canva p")
+
+var currentcode = ""
+
+var items = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+function capcha() {
+
+    currentcode = ""
+
+    for (var i = 0; i < 6; i++) {
+        var random_index = Math.floor(Math.random() * items.length)
+        currentcode = currentcode + items[random_index];
+    }
+
+    p.textContent = currentcode;
+
+}
+
+$("#refreshBtn").click(function () {
+    capcha();
+})
+
+$("#refreshBtn").keypress(function (event) {
+    if (event.key === "Enter") {
+        $('.last').click();
+    }
+})
+
+
+$(".last").click(function () {
+    var input = $("#text").val().trim().toUpperCase()
+    if (input === currentcode) {
+        alert("VERIFIED")
+    }
+    else {
+        alert("TRY AGAIN")
+        $('#text').val(''); // clear input
+        capcha();           // generate new code
+
+    }
+
+})
+
+capcha();
